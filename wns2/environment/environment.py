fdbee25f2cda01be2bbaf77595a36cdddd4201b3
csotaku <ben900926.cs09@nycu.edu.tw>
2024-12-24 15:35:31 +0800

    delete more files

--- wns2/environment/environment.py
+++ wns2/environment/environment.py
@@ -1,5 +1,4 @@
 import logging
-from wns2.userequipment.multipath_userequipment import MultiPathUserEquipment
 from wns2.userequipment.userequipment import UserEquipment
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
@@ -168,55 +167,7 @@ class Environment:
 
         for bs in self.bs_list:
             self.bs_list[bs].step()
-        '''       
-        # disconnect all the users, in order to reconnect them to the right AP and with the right datarate
-        for i in range(len(self.ue_list)):
-            self.ue_by_id(i).disconnect_all()
-        # call here the optimizator, taking the w from users (with data_generation_status and input_data_rate)    
-        N = len(self.connection_advertisement)
-        M = len(self.bs_list)
-        q = np.zeros(N)
-        w = np.zeros(N)
-        P = np.zeros((N, M))
-        for i in range(N):
-            ue = self.ue_by_id(self.connection_advertisement[i])
-            q[i] = ue.get_queue()
-            w[i] = ue.get_current_input_data_rate()
-            rsrp = ue.measure_rsrp()
-            for bs in rsrp:
-                P[i, bs] = 1
-        u_final = util.output_datarate_optimization(q, w, N, M, P, self.sampling_time, Q_max = MultiPathUserEquipment.MAX_QUEUE)
-        # set the desired data rate for all the users, connecting to the BSs
-        for i in range(N):
-            ue = self.ue_by_id(self.connection_advertisement[i])
-            connection_list = []
-            for bs in range(M):
-                if u_final[i, bs] > 0 and P[i, bs] == 1:
-                    ue.output_data_rate[bs] = u_final[i, bs]
-                    connection_list.append(bs)
-            ue.connect_bs(connection_list)
-        # move the drone AP according to the weighted average of the positions of the UEs connected to it
-        for drone_ap in self.drone_aps:
-            u_drone = []
-            x_ue = []
-            y_ue = []
-            for i in range(N):
-                if P[i, drone_ap] == 1:
-                    u_drone.append(u_final[i, drone_ap])
-                    pos = self.ue_by_id(i).get_position()
-                    x_ue.append(pos[0])
-                    y_ue.append(pos[1])
-            bs = self.bs_by_id(drone_ap)
-            u_0 = 0 # weight of the current position of the drone
-            x_drone = bs.get_position()[0]
-            y_drone = bs.get_position()[1]
-            z_drone = bs.get_position()[2]
-            new_drone_pos = util.build_drone_pos_ref(u_drone, x_ue, y_ue, u_0, x_drone, y_drone)
-            new_drone_pos = (new_drone_pos[0], new_drone_pos[1], z_drone)
-            #print("OLD DRONE POSITION: (%s, %s) ---> NEW DRONE POSITION: (%s, %s)" %(x_drone, y_drone, new_drone_pos[0], new_drone_pos[1]))
-            bs.move(new_drone_pos, speed=15)
-        '''
-        return            
+
 
     def render(self):
         if self.renderer != None:
